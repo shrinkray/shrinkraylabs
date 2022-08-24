803092001e6f7829fe1994af73c855d13f8653f6
Greg Miller <hello@gregmiller.io>
2022-08-24 19:57:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Hygraph SvelteKit Portfolio and Blog Starter
+
+> Minimalist Developer Portfolio and Blog built with SvelteKit,
+> Tailwind, daisyUI, and deployed to Vercel.
+
+[![Clone project](https://hygraph.com/button)](https://app.hygraph.com/clone/77e3a126472d443bad40b9e4cac85243?name=Developer%20Portfolio%20%26%20Blog)
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/spences10/developer-portfolio-and-blog&env=VITE_GRAPHQL_API)
+
+## The stack
+
+This is an example project generated with the Hygraph Developer
+Portfolio and Blog starter.
+
+- **[Hygraph](https://hygraph.com)**: Manage projects and blog
+  posts.
+- **[SvelteKit](https://kit.svelte.dev/)**: A powerful framework that
+  lets us query data and use endpoints for server side data.
+- **[Vercel](https://www.vercel.com/)**: For an intuitive Git based
+  workflow with seamless publishing.
+
+## How to use
+
+⚠TODO!
+
+Rename the `sample.env` file to `.env` and add your own Content API
+endpoint.
+
+```bash
+mv sample.env .env
+```
+
+Install dependencies and run local dev server:
+
+```bash
+npm i
+npm run dev
+```
---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,64 @@
+<script context="module">
+  import { page } from '$app/stores'
+  import Head from '$components/head.svelte'
+  import ProjectCard from '$components/project-card.svelte'
+  import { client } from '$lib/graphql-client'
+  import { projectsQuery } from '$lib/graphql-queries'
+  import {
+    fetchSiteMetadata,
+    siteMetadataStore,
+  } from '$stores/site-metadata'
+  import { onMount } from 'svelte'
+
+  export const load = async () => {
+    await fetchSiteMetadata()
+
+    const { projects } = await client.request(projectsQuery)
+
+    return {
+      props: {
+        projects,
+      },
+    }
+  }
+</script>
+
+<script>
+  export let projects
+  let pathname
+
+  onMount(async () => {
+    pathname = $page.url.pathname
+  })
+
+  const {
+    siteUrl,
+    name: siteName,
+    openGraphDefaultImage,
+  } = $siteMetadataStore || []
+</script>
+
+<Head
+  title={`Projects · ${siteName}`}
+  description={`A list of recent projects.`}
+  image={openGraphDefaultImage.url}
+  url={`${siteUrl}${pathname}`}
+/>
+
+<h1 class="font-bold mb-20 text-center text-5xl">
+  Recent Projects by Me
+</h1>
+
+<div
+  class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52"
+>
+  {#each projects as { name, slug, description, image }, index}
+    <ProjectCard
+      {name}
+      {description}
+      url={image[0].url}
+      {index}
+      {slug}
+    />
+  {/each}
+</div>
